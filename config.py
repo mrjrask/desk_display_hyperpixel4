f0dad9ebe56d8732f4c10ffbecd8e40c13edbba1
mrjrask <96755551+mrjrask@users.noreply.github.com>
2026-01-11 19:51:18 -0600

    Fix Rams logo mapping for Bears next game

--- config.py
+++ config.py
@@ -1011,7 +1011,7 @@ NFL_TEAM_ABBREVIATIONS = {
     "raiders": "lv",     "commanders": "was","saints": "no",
     "ravens": "bal",     "bengals": "cin",  "giants": "nyg",
     "steelers": "pit",   "eagles": "phi",   "packers": "gb",
-    "browns": "cle",     "49ers": "sf",
+    "browns": "cle",     "49ers": "sf",     "rams": "lar",
 }
 
 # VRNOF screen
